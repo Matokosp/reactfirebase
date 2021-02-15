43d00922b16ae3ebc0e8545a6c4aaac2a76aabd2
Matias <jmsalinas@uc.cl>
2021-02-15 17:01:11 -0500

    Updates

---
+++ precache-manifest.d5b989ce9e8eaa48ff018eb9b3923539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dc0ddd0484633e2215132b8a26cc95",
+    "url": "/reactfirebase/index.html"
+  },
+  {
+    "revision": "78a15e93b22c66438b9b",
+    "url": "/reactfirebase/static/css/main.df942c1b.chunk.css"
+  },
+  {
+    "revision": "ec4ca444beb14a7fea51",
+    "url": "/reactfirebase/static/js/2.0dec9cff.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/reactfirebase/static/js/2.0dec9cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a15e93b22c66438b9b",
+    "url": "/reactfirebase/static/js/main.421fefb7.chunk.js"
+  },
+  {
+    "revision": "2371a85e984dc3b9109b",
+    "url": "/reactfirebase/static/js/runtime-main.65ac1fb5.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/reactfirebase/static/media/background.4f646371.jpg"
+  }
+]);
